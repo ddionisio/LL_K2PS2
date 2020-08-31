4efc31e88e47be6c8451bd7d095f06615beb7270
David Dionisio <ddionisio@renegadeware.com>
2020-08-31 09:18:18 -0400

    initial project setup

---
+++ Assets/Scripts/Generated/LoLTypes.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace LoL
+{
+    // Generated from Attributes. Do not manual edit.
+    public static partial class Types
+    {
+        static Dictionary<string, Type> _Types = new Dictionary<string, Type>
+        {
+        };
+    }
+}
