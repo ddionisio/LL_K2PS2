011e2932bcec018a51813e0e200029a80c658062
David Dionisio <ddionisio@renegadeware.com>
2020-08-31 14:14:14 -0400

    more initial assets and setup

---
+++ Assets/Renegadeware/Scripts/GameStart.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Renegadeware.K2PS2 {
+    public class GameStart : MonoBehaviour {
+        [Header("Screen")]
+        public GameObject loadingGO;
+        public GameObject readyGO;
+
+        [Header("Title")]
+        [M8.Localize]
+        public string titleRef;
+        //public 
+
+        IEnumerator Start() {
+            while(M8.SceneManager.instance.isLoading)
+                yield return null;
+
+            //Loading
+            while(!LoLExt.LoLManager.instance.isReady)
+                yield return null;
+
+            //Title/Ready
+        }
+    }
+}
